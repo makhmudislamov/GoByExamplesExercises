2627e720b3acadbcd5cd8b5b0f1bd3b091e04baa
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-26 16:03:48 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Go By Example
+https://gobyexample.com/
---
+++ variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+}
