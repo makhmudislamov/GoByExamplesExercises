a90b10c7920c26419d761818b39802c49fb387c8
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-26 16:11:14 -0800

    variables and their declaration in go

--- variables.go
+++ variables.go
@@ -3,9 +3,23 @@ package main
 import "fmt"
 
 func main() {
+
 	var a = "initial"
 	fmt.Println(a)
 
+	// declaring multiple variable at once
 	var b, c int = 1, 2
 	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	// zero valued variable >> prints 0
+	var e int
+	fmt.Println(e)
+
+	// this is equal to f strin = "short"
+	// only inside function
+	f := "short"
+	fmt.Println(f)
 }
