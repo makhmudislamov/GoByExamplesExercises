186af17ddf1e4560e7ce8ba7d5c3b5e1f3740e74
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-26 16:41:42 -0800

    how for works in GoLang

--- constant.go
+++ constant.go
@@ -1,20 +1,20 @@
-package main
+// package main
 
-import "fmt"
-import "math"
+// import "fmt"
+// import "math"
 
-// const supports strin, boolean, char and numeric vals
-const s string = "constant"
+// // const supports strin, boolean, char and numeric vals
+// const s string = "constant"
 
-func main() {
-	fmt.Println(s)
-	// can be inside or outside a func just like var
-	const n = 500000000
+// func main() {
+// 	fmt.Println(s)
+// 	// can be inside or outside a func just like var
+// 	const n = 500000000
 
-	const d = 3e20 / n
-	fmt.Println(d)
+// 	const d = 3e20 / n
+// 	fmt.Println(d)
 
-	fmt.Println(int64(d))
+// 	fmt.Println(int64(d))
 
-	fmt.Println(math.Sin(n))
-}
+// 	fmt.Println(math.Sin(n))
+// }
